a1812770580343ba34af92d5689b26c606f13b60
Telmo Menezes <telmo@telmomenezes.com>
2011-04-15 15:54:18 +0200

    created setup.py and synpython.c

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+setup.py
+
+Created by Telmo Menezes on 2011-04-15.
+Copyright (c) 2011 Telmo Menezes. All rights reserved.
+"""
+
+from distutils.core import setup, Extension
+
+module1 = Extension('syn',
+                    define_macros = [('MAJOR_VERSION', '1'),
+                                     ('MINOR_VERSION', '0')],
+                    libraries = ['m'],
+                    sources = ['src/synpython.c'])
+
+setup (name = 'syn',
+       version = '1.0',
+       description = 'TBD',
+       author = 'Telmo Menezes',
+       author_email = 'telmo@telmomenezes.com',
+       url = 'http://telmomenezes.com',
+       long_description = '''
+TBD.
+''',
+       ext_modules = [module1])
---
+++ src/synpython.c
@@ -0,0 +1,24 @@
+#include <Python.h>
+
+static PyObject *MyCommand(PyObject *self, PyObject *args)
+{
+    PyObject *result = NULL;
+    long a, b;
+
+    if (PyArg_ParseTuple(args, "ii", &a, &b)) {
+      result = Py_BuildValue("i", a + b);
+    }
+    
+    return result;
+}
+
+static PyMethodDef methods[] = {
+  {"xpto", MyCommand, METH_VARARGS, "TBD"},
+  {NULL, NULL, 0, NULL},
+};
+
+PyMODINIT_FUNC initsyn(void)
+{
+    Py_InitModule("syn", methods);
+}
+  
