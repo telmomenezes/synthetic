301c87bb27a963a34db286d4e70c31b9e74f65f5
Telmo Menezes <telmo@telmomenezes.com>
2011-04-16 00:56:13 +0200

    converted emm.cpp to c

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ module1 = Extension('syn',
                                      ('MINOR_VERSION', '0')],
                     libraries = ['m'],
                     include_dirs = ['src'],
-                    sources = ['src/synpython.c', 'src/edge.c', 'src/node.c'])
+                    sources = ['src/synpython.c', 'src/edge.c', 'src/node.c', 'src/emd.c'])
 
 setup (name = 'syn',
        version = '1.0',
--- src/emd.c
+++ src/emd.c
@@ -87,7 +87,7 @@ static double init(signature_t *Signature1, signature_t *Signature2,
 static void findBasicVariables(node1_t *U, node1_t *V);
 static int isOptimal(node1_t *U, node1_t *V);
 static int findLoop(node2_t **Loop);
-static void newSol();
+static void newSol(void);
 static void russel(double *S, double *D);
 static void addBasicVariable(int minI, int minJ, double *S, double *D, 
 			     node1_t *PrevUMinI, node1_t *PrevVMinJ,
@@ -134,7 +134,7 @@ double emd(signature_t *Signature1, signature_t *Signature2,
   double totalCost;
   double w;
   node2_t *XP;
-  flow_t *FlowP;
+  flow_t *FlowP = NULL;
   node1_t U[MAX_SIG_SIZE1], V[MAX_SIG_SIZE1];
 
   w = init(Signature1, Signature2, Dist);
@@ -436,7 +436,9 @@ static void findBasicVariables(node1_t *U, node1_t *V)
 static int isOptimal(node1_t *U, node1_t *V)
 {    
   double delta, deltaMin;
-  int i, j, minI, minJ;
+  int i, j;
+  int minI = 0;
+  int minJ = 0;
 
   /* FIND THE MINIMAL Cij-Ui-Vj OVER ALL i,j */
   deltaMin = EMD_INFINITY;
@@ -483,7 +485,8 @@ static void newSol()
     int i, j, k;
     double xMin;
     int steps;
-    node2_t *Loop[2*MAX_SIG_SIZE1], *CurX, *LeaveX;
+    node2_t *Loop[2*MAX_SIG_SIZE1], *CurX;
+    node2_t *LeaveX = NULL;
  
 #if DEBUG_LEVEL > 3
     printf("EnterX = (%d,%d)\n", _EnterX->i, _EnterX->j);
@@ -652,13 +655,17 @@ static int findLoop(node2_t **Loop)
 **********************/
 static void russel(double *S, double *D)
 {
-  int i, j, found, minI, minJ;
+  int i, j, found;
+  int minI = 0;
+  int minJ = 0;
   double deltaMin, oldVal, diff;
   double Delta[MAX_SIG_SIZE1][MAX_SIG_SIZE1];
   node1_t Ur[MAX_SIG_SIZE1], Vr[MAX_SIG_SIZE1];
   node1_t uHead, *CurU, *PrevU;
   node1_t vHead, *CurV, *PrevV;
-  node1_t *PrevUMinI, *PrevVMinJ, *Remember;
+  node1_t *PrevUMinI = NULL;
+  node1_t *PrevVMinJ = NULL;
+  node1_t *Remember = NULL;
 
   /* INITIALIZE THE ROWS LIST (Ur), AND THE COLUMNS LIST (Vr) */
   uHead.Next = CurU = Ur;
