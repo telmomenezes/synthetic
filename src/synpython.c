f4e3ecf1894a38f02ea64fa2f82da461d8e9fdfc
Telmo Menezes <telmo@telmomenezes.com>
2011-05-13 13:21:18 +0200

    add node and edge functions included in syn.core

--- src/synpython.c
+++ src/synpython.c
@@ -4,8 +4,6 @@
 #include "network.h"
 #include "histogram2d.h"
 
-syn_net *syn_create_net(void);
-void syn_destroy_net(syn_net *net);
 
 static PyObject *pysyn_create_net(PyObject *self, PyObject *args)
 {
@@ -26,6 +24,40 @@ static PyObject *pysyn_destroy_net(PyObject *self, PyObject *args)
     }
     
     PyObject *result = Py_BuildValue("");
+    return result;
+}
+
+static PyObject *pysyn_add_node(PyObject *self, PyObject *args)
+{
+    long p;
+    unsigned int type;
+    syn_net *net;
+    syn_node *node = NULL;
+
+    if (PyArg_ParseTuple(args, "ii", &p, &type)) {
+      net = (syn_net *)p;
+      node = syn_add_node(net, type);
+    }
+
+    PyObject *result = Py_BuildValue("i", (long)node);
+    return result;
+}
+
+static PyObject *pysyn_add_edge_to_net(PyObject *self, PyObject *args)
+{
+    long p1, p2, p3;
+    syn_net *net;
+    syn_node *orig, *targ;
+    int res = 0;
+
+    if (PyArg_ParseTuple(args, "iii", &p1, &p2, &p3)) {
+      net = (syn_net *)p1;
+      orig = (syn_node *)p2;
+      targ = (syn_node *)p3;
+      res = syn_add_edge_to_net(net, orig, targ);
+    }
+
+    PyObject *result = Py_BuildValue("b", res);
     return result;
 }
 
@@ -134,6 +166,8 @@ static PyObject *pysyn_histogram2d_get_value(PyObject *self, PyObject *args)
 static PyMethodDef methods[] = {
     {"create_net", pysyn_create_net, METH_VARARGS, "Create network."},
     {"destroy_net", pysyn_destroy_net, METH_VARARGS, "Destroy network."},
+    {"add_node", pysyn_add_node, METH_VARARGS, "Add node to network."},
+    {"add_edge_to_net", pysyn_add_edge_to_net, METH_VARARGS, "Add edge to network."},
     {"compute_evc", pysyn_compute_evc, METH_VARARGS, "Compute EVC."},
     {"write_evc", pysyn_write_evc, METH_VARARGS, "Write EVC."},
     {"print_net_info", pysyn_print_net_info, METH_VARARGS, "Print net info."},
