79fde1482ba7ed65a534cb6873e3a20714ae4bdb
Telmo Menezes <telmo@telmomenezes.com>
2011-06-27 13:26:38 +0200

    implemented load_net in net.py

--- pysrc/net.py
+++ pysrc/net.py
@@ -39,3 +39,29 @@ class Net:
         self.safe_execute("ALTER TABLE edge ADD COLUMN orig INTEGER")
         self.safe_execute("ALTER TABLE edge ADD COLUMN targ INTEGER")
         self.safe_execute("ALTER TABLE node ADD COLUMN ts REAL")
+
+    def load_net(self, min_ts=-1, max_ts=-1):
+        cur = self.conn.cursor()
+    
+        net = create_net()
+
+        cur.execute("SELECT id FROM node")
+        
+        nodes = {}
+        for row in cur:
+            nodes[row[0]] = add_node(net, 0)
+
+        if min_ts >= 0:
+            if max_ts >= 0:
+                cur.execute("SELECT orig, targ FROM node WHERE ts>=%f AND ts<%f" % (min_ts, max_ts))
+            else:
+                cur.execute("SELECT orig, targ FROM node WHERE ts>=%f" % (min_ts))
+        else:
+            cur.execute("SELECT orig, targ, ts FROM node")
+
+        for row in cur:
+            add_edge_to_net(net, nodes[row[0]], nodes[row[1]], row[2])
+
+        cur.close()
+
+        return net
