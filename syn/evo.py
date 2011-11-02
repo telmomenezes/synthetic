33e363f11e52cf7934efe18b584f1fcccf8accf5
Telmo Menezes <telmo@telmomenezes.com>
2011-11-02 13:38:59 +0100

    tweaking evo

--- syn/evo.py
+++ syn/evo.py
@@ -23,7 +23,7 @@ class Evo:
         self.syn_net = targ_net.load_net()
         self.nodes = net_node_count(self.syn_net)
         self.edges = net_edge_count(self.syn_net)
-        self.nodes = 5000
+        self.nodes = 1000
         self.edges = self.nodes * (net_edge_count(self.syn_net) / net_node_count(self.syn_net))
         self.max_cycles = self.edges * 100
 
