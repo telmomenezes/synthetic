c9a50573a022467713f30240cdf212f76997ea13
Telmo Menezes <telmo@telmomenezes.com>
2011-12-15 21:41:51 +0100

    created info.py

---
+++ scripts/info.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+__author__ = "Telmo Menezes (telmo@telmomenezes.com)"
+__date__ = "Dec 2011"
+
+
+from syn.net import Net
+import sys
+
+
+if __name__ == '__main__':
+    net = Net(sys.argv[1])
+    syn_net = net.load_net()
+    nodes = net_node_count(syn_net)
+    edges = net_edge_count(syn_net)
+    print 'Nodes: ', nodes
+    print 'Edges: ', edges
