a1ce5ce41da5b8e746aebcc2633a7492dcbc1080
Telmo Menezes <telmo@telmomenezes.com>
2012-01-17 16:14:18 +0100

    express computational limits in terms of max nodes

--- syn/evo.py
+++ syn/evo.py
@@ -13,28 +13,23 @@ from syn.drmap import *
 
 
 class Evo:
-    def __init__(self, targ_net, max_effort=5, map_limit=7.0, bins=10):
+    def __init__(self, targ_net, max_nodes=500, map_limit=7.0, bins=10):
         self.targ_net = targ_net
         self.population = []
 
         self.syn_net = targ_net.load_net()
         self.nodes = net_node_count(self.syn_net)
-        self.edges = net_edge_count(self.syn_net)
-        
-        # edge to node ratio
-        en_ratio = float(net_edge_count(self.syn_net)) / float(net_node_count(self.syn_net))
-        
-        # max effort
-        me = float(max_effort * 1000000) 
+        self.edges = net_edge_count(self.syn_net) 
 
-        # total effort
-        e = en_ratio * (self.nodes * self.nodes)
-
-        # check if max effort was reached
-        if e > me:
-            n = math.sqrt(me / en_ratio)
-            self.nodes = int(n)
-            self.edges = int(n * en_ratio)
+        # check if max nodes is reached
+        if self.nodes > max_nodes:
+            self.nodes = max_nodes
+            
+            # edge to node ratio
+            en_ratio = float(net_edge_count(self.syn_net)) / float(net_node_count(self.syn_net))
+            
+            self.edges = int(float(max_nodes) * en_ratio)
+        
         self.map_limit = map_limit
         self.bins = bins
 
