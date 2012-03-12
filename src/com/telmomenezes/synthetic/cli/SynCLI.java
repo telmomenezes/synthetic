c9596c73fbbf9cf072969cd23a3a9136269cbf0c
Telmo Menezes <telmo@telmomenezes.com>
2012-03-12 17:42:56 +0100

    starting cli

---
+++ src/com/telmomenezes/synthetic/cli/SynCLI.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.telmomenezes.synthetic.cli;
+
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.PosixParser;
+
+/**
+ * @author telmo
+ *
+ */
+public class SynCLI {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        CommandLineParser parser = new PosixParser();
+
+    }
+
+}
