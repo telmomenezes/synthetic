23e523b1d3820fe91cb9da4bdc0f4410732ed793
Telmo Menezes <telmo@telmomenezes.com>
2012-12-12 15:21:33 +0100

    correcting full generator bug

--- src/com/telmomenezes/synthetic/cli/SynCLI.java
+++ src/com/telmomenezes/synthetic/cli/SynCLI.java
@@ -31,7 +31,7 @@ public class SynCLI {
     public void run(String[] args) {
         //args = new String[]{"evo", "-inet", "ownership.txt", "-odir", "test"};
     	//args = new String[]{"evo", "-inet", "/Users/telmo/Desktop/Networks/polblogs/polblogs.gml", "-odir", "test"};
-    	args = new String[]{"evo", "-inet", "celegansneural.gml", "-odir", "test", "-gentype", "full"};
+    	//args = new String[]{"evo", "-inet", "celegansneural.gml", "-odir", "test", "-gentype", "full"};
         
         CommandLineParser parser = new GnuParser();
         options = new Options();
