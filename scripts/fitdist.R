429ce4487765433648f240ba11e2078735ae25f8
Telmo Menezes <telmo@telmomenezes.com>
2013-05-23 16:19:03 +0200

    improving fitdist.R

--- scripts/fitdist.R
+++ scripts/fitdist.R
@@ -17,13 +17,13 @@ corAvg <- cor(dataFit$prog_dist, dataFit$fit_avg)
 corMaxTest <- cor.test(dataFit$prog_dist, dataFit$fit_max)
 corAvgTest <- cor.test(dataFit$prog_dist, dataFit$fit_avg)
 
-maxText <- paste("Pearson's correlation: ", round(corMax, digits=2))
+maxText <- paste("correlation: ", round(corMax, digits=2))
 maxText <- paste(maxText, "\np-value: ", round(corMaxTest$p.value, digits=5))
 
-avgText <- paste("Pearson's correlation: ", round(corAvg, digits=2))
+avgText <- paste("correlation: ", round(corAvg, digits=2))
 avgText <- paste(avgText, "\np-value: ", round(corAvgTest$p.value, digits=5))
 
-p1 <- ggplot(dataFit, aes(x=prog_dist, y=fit_max, label=prog)) + geom_point() + geom_text(vjust=0) + geom_smooth(method="lm", fill=NA)
+p1 <- ggplot(dataFit, aes(x=prog_dist, y=fit_max, label=prog)) + geom_point() + geom_text(aes(colour=group)) + geom_smooth(method="lm", fill=NA)
 p1 <- p1 + theme(axis.line = element_line(colour = "black"),
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
@@ -32,10 +32,11 @@ p1 <- p1 + theme(axis.line = element_line(colour = "black"),
 p1 <- p1 + xlab("program distance")
 p1 <- p1 + ylab("fitness (max)")
 p1 <- p1 + geom_text(aes(x2, y2, label=texthere), 
-          data.frame(x2=0.004, y2=0.8, texthere=maxText))
+          data.frame(x2=0.006, y2=0.8, texthere=maxText))
+p1 <- p1 + opts(legend.position="none")
 
 
-p2 <- ggplot(dataFit, aes(x=prog_dist, y=fit_avg, label=prog)) + geom_point() + geom_text(vjust=0) + geom_smooth(method="lm", fill=NA)
+p2 <- ggplot(dataFit, aes(x=prog_dist, y=fit_avg, label=prog)) + geom_point() + geom_text(aes(colour=group)) + geom_smooth(method="lm", fill=NA)
 p2 <- p2 + theme(axis.line = element_line(colour = "black"),
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
@@ -44,8 +45,34 @@ p2 <- p2 + theme(axis.line = element_line(colour = "black"),
 p2 <- p2 + xlab("program distance")
 p2 <- p2 + ylab("fitness (mean)")
 p2 <- p2 + geom_text(aes(x2, y2, label=texthere), 
-          data.frame(x2=0.004, y2=0.6, texthere=avgText))
+          data.frame(x2=0.006, y2=0.6, texthere=avgText))
+p2 <- p2 + opts(legend.position="none")
 
-plots = arrangeGrob(p1, p2)
-pdf(file=pdfFile, height=10, width=10)
+
+dataFit$group2 <- with(dataFit, relevel(group, "near"))
+
+p3 <- ggplot(dataFit, aes(x=group2, y=fit_max)) + geom_boxplot(aes(colour=group))
+p3 <- p3 + theme(axis.line = element_line(colour = "black"),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.border = element_blank(),
+    panel.background = element_blank()) 
+p3 <- p3 + xlab("group")
+p3 <- p3 + ylab("fitness (max)")
+p3 <- p3 + opts(legend.position="none")
+
+
+p4 <- ggplot(dataFit, aes(x=group2, y=fit_avg)) + geom_boxplot(aes(colour=group))
+p4 <- p4 + theme(axis.line = element_line(colour = "black"),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.border = element_blank(),
+    panel.background = element_blank()) 
+p4 <- p4 + xlab("group")
+p4 <- p4 + ylab("fitness (mean)")
+p4 <- p4 + opts(legend.position="none")
+
+
+plots = arrangeGrob(p1, p2, p3, p4)
+pdf(file=pdfFile, height=8, width=10)
 grid.arrange(plots, heights=c(1), nrow=1)
