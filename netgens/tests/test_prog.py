af976d59e399ec8d64f4dce518197c334afcdfb4
Telmo Menezes <telmo@telmomenezes.comm>
2018-11-09 02:13:23 +0000

    more tests

--- netgens/tests/test_prog.py
+++ netgens/tests/test_prog.py
@@ -1,4 +1,5 @@
 import unittest
+import numpy as np
 from netgens.progs.prog import parse
 
 
@@ -33,6 +34,69 @@ class TestProg(unittest.TestCase):
         self.assertEqual(str(prog), '(== 1.0 2.0 0.0 1.0)')
         self.assertEqual(prog.eval(), 1.)
 
+    def test_prog_grt_true(self):
+        prog = parse('(> 2 1 0 1)', [])
+        self.assertEqual(str(prog), '(> 2.0 1.0 0.0 1.0)')
+        self.assertEqual(prog.eval(), 0.)
+
+    def test_prog_grt_false(self):
+        prog = parse('(> -2 1 0 1)', [])
+        self.assertEqual(str(prog), '(> -2.0 1.0 0.0 1.0)')
+        self.assertEqual(prog.eval(), 1.)
+
+    def test_prog_lrt_true(self):
+        prog = parse('(< -2 1 0 1)', [])
+        self.assertEqual(str(prog), '(< -2.0 1.0 0.0 1.0)')
+        self.assertEqual(prog.eval(), 0.)
+
+    def test_prog_lrt_false(self):
+        prog = parse('(< 2 1 0 1)', [])
+        self.assertEqual(str(prog), '(< 2.0 1.0 0.0 1.0)')
+        self.assertEqual(prog.eval(), 1.)
+
+    def test_prog_exp(self):
+        prog = parse('(EXP 1)', [])
+        self.assertEqual(str(prog), '(EXP 1.0)')
+        np.testing.assert_almost_equal(prog.eval(), 2.718281828459045)
+
+    def test_prog_log(self):
+        prog = parse('(LOG 2)', [])
+        self.assertEqual(str(prog), '(LOG 2.0)')
+        np.testing.assert_almost_equal(prog.eval(), 0.6931471805599453)
+
+    def test_prog_abs(self):
+        prog = parse('(ABS -5)', [])
+        self.assertEqual(str(prog), '(ABS -5.0)')
+        self.assertEqual(prog.eval(), 5.)
+
+    def test_prog_min(self):
+        prog = parse('(MIN -5 5)', [])
+        self.assertEqual(str(prog), '(MIN -5.0 5.0)')
+        self.assertEqual(prog.eval(), -5.)
+
+    def test_prog_max(self):
+        prog = parse('(MAX -5 5)', [])
+        self.assertEqual(str(prog), '(MAX -5.0 5.0)')
+        self.assertEqual(prog.eval(), 5.)
+
+    def test_prog_pow(self):
+        prog = parse('(^ 2.5 3.5)', [])
+        self.assertEqual(str(prog), '(^ 2.5 3.5)')
+        np.testing.assert_almost_equal(prog.eval(), 24.705294220065465)
+
+    def test_prog_aff_true(self):
+        prog = parse('(AFF 2 0 1)', ['id1', 'id2'])
+        prog.vars[0] = 10
+        prog.vars[1] = 20
+        self.assertEqual(str(prog), '(AFF 2.0 0.0 1.0)')
+        self.assertEqual(prog.eval(), 0.)
+
+    def test_prog_aff_false(self):
+        prog = parse('(AFF 3 0 1)', ['id1', 'id2'])
+        prog.vars[0] = 10
+        prog.vars[1] = 20
+        self.assertEqual(str(prog), '(AFF 3.0 0.0 1.0)')
+        self.assertEqual(prog.eval(), 1.)
 
 if __name__ == '__main__':
     unittest.main()
