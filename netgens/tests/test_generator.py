7a5358c7294bba95e9b9a15fd6e89a650a42da9e
Telmo Menezes <telmo@telmomenezes.comm>
2018-11-27 22:28:16 +0100

    simple generator test

---
+++ netgens/tests/test_generator.py
@@ -0,0 +1,14 @@
+import unittest
+from netgens.consts import *
+from netgens.generator import generator_from_prog_str
+
+
+class TestGenerator(unittest.TestCase):
+    def test_gen_random(self):
+        gen = generator_from_prog_str('1', True)
+        net = gen.run(100, 10, DEFAULT_SAMPLE_RATE)
+        self.assertEqual(len(net.vs), 100)
+
+
+if __name__ == '__main__':
+    unittest.main()
