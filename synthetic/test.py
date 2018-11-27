078252f8adc53eb757ac3d0373b9207a9b2d5c1e
Telmo Menezes <telmo@telmomenezes.comm>
2018-11-27 23:04:32 +0100

    removing test script

--- synthetic/test.py
+++
@@ -1,7 +0,0 @@
-from synthetic.consts import *
-from synthetic.generator import generator_from_prog_str
-
-
-gen = generator_from_prog_str('1', True)
-net = gen.run(100, 100, DEFAULT_SAMPLE_RATE)
-print(len(net.es))
