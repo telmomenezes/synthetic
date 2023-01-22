843f658e9e07fa0cbaf67e8fb1646aeb8a1802bc
Telmo Menezes <telmo@telmomenezes.net>
2023-01-22 23:44:02 +0100

    higher number of normalization samples

--- synthetic/commands/fit.py
+++ synthetic/commands/fit.py
@@ -1,12 +1,11 @@
-from synthetic.consts import (DEFAULT_SAMPLE_RATE, DEFAULT_BINS,
-                              DEFAULT_MAX_DIST, DEFAULT_RUNS, DEFAULT_GEN_TYPE,
+from synthetic.consts import (DEFAULT_SAMPLE_RATE, DEFAULT_BINS, DEFAULT_MAX_DIST, DEFAULT_RUNS, DEFAULT_GEN_TYPE,
                               DEFAULT_NORM_SAMPLES)
 from synthetic.net import load_net
 from synthetic.generator import create_generator, load_generator
 from synthetic.distances import DistancesToNet, Norm
-from synthetic.commands.command import (Command, arg_with_default,
-                                        get_stat_dist_types)
+from synthetic.commands.command import Command, arg_with_default, get_stat_dist_types
 from statistics import mean
+
 
 class Fit(Command):
     def __init__(self, cli_name):
@@ -14,8 +13,7 @@ class Fit(Command):
         self.name = 'fit'
         self.description = 'mean fitness of several runs of a generator'
         self.mandatory_args = ['inet', 'prg']
-        self.optional_args = ['undir', 'sr', 'bins', 'maxdist', 'runs',
-                              'gen_type']
+        self.optional_args = ['undir', 'sr', 'bins', 'maxdist', 'runs', 'gen_type', 'rw']
 
     def run(self, args):
         self.error_msg = None
@@ -25,6 +23,7 @@ class Fit(Command):
         sr = arg_with_default(args, 'sr', DEFAULT_SAMPLE_RATE)
         bins = arg_with_default(args, 'bins', DEFAULT_BINS)
         max_dist = arg_with_default(args, 'maxdist', DEFAULT_MAX_DIST)
+        rw = args['rw']
         directed = not args['undir']
         runs = arg_with_default(args, 'runs', DEFAULT_RUNS)
         gen_type = arg_with_default(args, 'gentype', DEFAULT_GEN_TYPE)
@@ -40,8 +39,7 @@ class Fit(Command):
 
         # create fitness calculator
         # TODO: norm samples configurable
-        fitness = DistancesToNet(net, get_stat_dist_types(args), bins,
-                                 max_dist, norm=Norm.ER_MEAN_RATIO,
+        fitness = DistancesToNet(net, get_stat_dist_types(args), bins, max_dist, rw, norm=Norm.ER_MEAN_RATIO,
                                  norm_samples=DEFAULT_NORM_SAMPLES)
 
         fit_maxes = []
@@ -50,7 +48,7 @@ class Fit(Command):
             print('run #{}'.format(i))
 
             gen = load_generator(prog, directed, gen_type)
-            synth_net = gen.run(net.vcount(), net.ecount(), sr)
+            synth_net = gen.run(net.graph.vcount(), net.graph.ecount(), sr)
             distances = fitness.compute(synth_net)
             fit_max=max(distances)
             fit_mean=mean(distances)
@@ -58,8 +56,7 @@ class Fit(Command):
             fit_maxes.append(fit_max)
             fit_means.append(fit_mean)
 
-            print('fitness (max): {}; fitness (mean): {}'.format(fit_max,
-                                                                 fit_mean))
+            print('fitness (max): {}; fitness (mean): {}'.format(fit_max, fit_mean))
             print([stat_type.name for stat_type in fitness.stat_types])
             print(distances)
 
@@ -73,14 +70,10 @@ class Fit(Command):
 
         print('\n\n')
 
-        print('mean fitness (max): {}; '
-              'min fitness (max): {}; '
-              'max fitness (max): {}'.format(mean_fit_max, min_fit_max,
-                                             max_fit_max))
-        print('mean fitness (mean): {}; '
-              'min fitness (mean): {}; '
-              'max fitness (mean): {}'.format(mean_fit_mean, min_fit_mean,
-                                              max_fit_mean))
+        print('mean fitness (max): {}; min fitness (max): {}; max fitness (max): {}'.format(
+            mean_fit_max, min_fit_max, max_fit_max))
+        print('mean fitness (mean): {}; min fitness (mean): {}; max fitness (mean): {}'.format(
+            mean_fit_mean, min_fit_mean, max_fit_mean))
 
         print('done.')
 
